aac695da0ad1029e17547f92945cdb3c850c5e62
amorfniy <2023kazantsev.ss@student.letovo.ru>
2022-01-18 15:46:52 +0300

    Problem 6

---
+++ src/problem6/Main.java
@@ -0,0 +1,23 @@
+package problem6;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner reader = new Scanner(System.in);
+        int n = reader.nextInt();
+        int[] numbers = reader.nextLine();
+
+        for(int i = 0; i <= n; i++){
+            if (numbers[i] == 0) {
+                int b = numbers[n-1];
+                numbers[i] = numbers[b];
+                b--;
+            }
+        }
+        numbers.toString();
+        System.out.println(numbers);
+
+
+    }
+}
